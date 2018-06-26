d746c123163b4176bea1fe8ae6c37d6674fe7478
mde-Odoo <32649748+mde-MateusDelai@users.noreply.github.com>
2018-06-26 19:25:14 -0400

    Update sale.order.py

--- models/sale.order.py
+++ models/sale.order.py
@@ -6,3 +6,6 @@ class SaleOrder(models.Model):
     _sql_constraints = [
         ('unique_po','UNIQUE(client_order_ref)',('This is not an Unique PO.'))
     ]
+    _sql_constraints = [
+        ('value_company_uniq', 'unique (client_order_ref)', 'This attribute value already exists !')
+    ]
