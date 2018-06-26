f03ad42240d44cbb9ac9cebd12dc4ef4eda92bdd
mde-Odoo <32649748+mde-MateusDelai@users.noreply.github.com>
2018-06-26 19:21:02 -0400

    Update sale.order.py

--- models/sale.order.py
+++ models/sale.order.py
@@ -4,7 +4,5 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     _sql_constraints = [
-        ('unique_po',
-         'UNIQUE (client_order_ref)',
-         _('This is not an Unique PO.'))
+        ('unique_po','UNIQUE(client_order_ref)',('This is not an Unique PO.'))
     ]
