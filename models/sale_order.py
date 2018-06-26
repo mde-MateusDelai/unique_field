e944ce5e290ddcfc4ad2198ae8a53a0fd1195d62
mde-Odoo <32649748+mde-MateusDelai@users.noreply.github.com>
2018-06-26 19:47:15 -0400

    Update sale_order.py

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,11 +1,9 @@
 from openerp import api, models
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+class SaleOrders(models.Model):
+    _inherit = "sale"
 
     _sql_constraints = [
         ('unique_po','UNIQUE(client_order_ref)',('This is not an Unique PO.'))
     ]
-    _sql_constraints = [
-        ('value_company_uniq', 'unique (client_order_ref)', 'This attribute value already exists !')
-    ]
+
