b51518a5bdb07ba5fb3a97d81851e5e341907a64
mde-Odoo <32649748+mde-MateusDelai@users.noreply.github.com>
2018-06-26 19:03:47 -0400

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Mateus - Mateus Delai <mde@odoo.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Unique PO per Customer',
+    'version': '9.0.1.0.0',
+    'category': 'Hidden',
+    'author': 'Mateus Delai',
+    'website': "",
+    'license': 'AGPL-3',
+    'depends': ['base', ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False
+}
