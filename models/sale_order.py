55defa857ffffd6888be3e6daa0aa639ab8fc561
mde-Odoo <32649748+mde-MateusDelai@users.noreply.github.com>
2018-06-26 20:06:36 -0400

    Update sale_order.py

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,16 +6,6 @@ from openerp.exceptions import UserError, RedirectWarning, ValidationError
 class sale_order(models.Model):
 	_inherit = ['sale.order']
 
-	client_order_ref = fields.Char(string='Customer Reference', copy=False, required=True)
-
-	@api.onchange('client_order_ref')
-	def _onchange_client_order_ref(self):
-		if self.env['sale.order'].search([('partner_id', '=', self.partner_id.id),\
-			('client_order_ref', '=', self.client_order_ref),\
-			('state', '!=', self.state)]):
-					warning_mess = {
-						'title': _('ValidationError'),
-						'message' : _('La referencia ya se ha usado para este cliente.')
-					}
-					return {'warning': warning_mess}
-		return {}
+_sql_constraints = [
+        ('code_company_uniq', 'unique (client_order_ref)', 'The code of the account must be unique per company !')
+    ]
