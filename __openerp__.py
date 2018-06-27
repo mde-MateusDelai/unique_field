984d0edee260992512f275c228a4a1865bb82e94
mde-Odoo <32649748+mde-MateusDelai@users.noreply.github.com>
2018-06-26 20:10:04 -0400

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Unique PO per Customer',
+    'name': 'Unique PO per Customer a',
     'version': '9.0.1.0.0',
     'category': 'Denker',
     'author': 'Mateus Delai',
