f2cdb5196c3bec20406ebaa3c649051635b8bb5f
mde-Odoo <32649748+mde-MateusDelai@users.noreply.github.com>
2018-06-26 20:25:25 -0400

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "New Module",
+    'version': '1.1',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+  'depends': ['sale'],
+    'data': [
+        'sale_order.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
